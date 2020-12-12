da0204aefcf3e7d61863cd0745352f0708b1d702
nikxtaco <nikitamenon2510@gmail.com>
2020-12-12 09:46:23 +0530

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,51 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, redirect, url_for
+import db
+import json
 app = Flask(__name__)
+# import views
+
+@app.route("/")
+def index():
+    return "Hello world"
+
+@app.route('/addname/<name>/')
+def addname(name):
+    db.users_collection.insert_one({"name": name.lower()})
+    return redirect(url_for('getnames'))
+
+@app.route('/getnames/')
+def getnames():
+    names_json = []
+    if db.users_collection.find({}):
+        for name in db.users_collection.find({}).sort("name"):
+            names_json.append({"name": name['name'], "id": str(name['_id'])})
+    return json.dumps(names_json)
+
+#test to insert data to the data base
+@app.route("/test")
+def test():
+    db.products_collection.insert_one({"name": "John"})
+    return "Connected to the data base!"
+
+# the real deal
+@app.route('/getproducts/', methods=['GET'])
+def getproducts():
+    products_json = []
+    if db.products_collection.find({}):
+        for product in db.products_collection.find({}).sort("name"):
+            products_json.append({"id": str(product['_id']), "name": product['name']})
+    return json.dumps(products_json)
+
+@app.route('/addproduct/', methods=['POST'])
+def addproduct():
+    product = {
+        'name': request.json['name'],
+        'desc': request.json['desc']
+    }
+    db.products_collection.insert_one(product)
+    return redirect(url_for('getproducts'))
+
+#-------------------------------------------
 
 @app.route('/getmsg/', methods=['GET'])
 def respond():
@@ -41,9 +87,9 @@ def post_something():
         })
 
 # A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+# @app.route('/')
+# def index():
+#     return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
