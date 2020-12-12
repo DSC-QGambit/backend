d5622a77050c14a57c1f7e027e62afc6d318d197
nikxtaco <nikitamenon2510@gmail.com>
2020-12-12 12:05:23 +0530

    update

--- app.py
+++ app.py
@@ -1,49 +1,59 @@
 from flask import Flask, request, jsonify, redirect, url_for
-import db
+# import db
 import json
 app = Flask(__name__)
-# import views
+from views import index
 
 @app.route("/")
-def index():
-    return "Hello world"
+def pg():
+    index()
+    print("hihjhhk")
+    return index()
 
-@app.route('/addname/<name>/')
-def addname(name):
-    db.users_collection.insert_one({"name": name.lower()})
-    return redirect(url_for('getnames'))
+# @app.route('/get-top-news/', methods=['GET'])
+# def getproducts():
+#     products_json = []
+#     if db.products_collection.find({}):
+#         for product in db.products_collection.find({}).sort("name"):
+#             products_json.append({"id": str(product['_id']), "name": product['name']})
+#     return json.dumps(products_json)
 
-@app.route('/getnames/')
-def getnames():
-    names_json = []
-    if db.users_collection.find({}):
-        for name in db.users_collection.find({}).sort("name"):
-            names_json.append({"name": name['name'], "id": str(name['_id'])})
-    return json.dumps(names_json)
+# @app.route('/addname/<name>/')
+# def addname(name):
+#     db.users_collection.insert_one({"name": name.lower()})
+#     return redirect(url_for('getnames'))
 
-#test to insert data to the data base
-@app.route("/test")
-def test():
-    db.products_collection.insert_one({"name": "John"})
-    return "Connected to the data base!"
+# @app.route('/getnames/')
+# def getnames():
+#     names_json = []
+#     if db.users_collection.find({}):
+#         for name in db.users_collection.find({}).sort("name"):
+#             names_json.append({"name": name['name'], "id": str(name['_id'])})
+#     return json.dumps(names_json)
 
-# the real deal
-@app.route('/getproducts/', methods=['GET'])
-def getproducts():
-    products_json = []
-    if db.products_collection.find({}):
-        for product in db.products_collection.find({}).sort("name"):
-            products_json.append({"id": str(product['_id']), "name": product['name']})
-    return json.dumps(products_json)
+# #test to insert data to the data base
+# @app.route("/test")
+# def test():
+#     db.products_collection.insert_one({"name": "John"})
+#     return "Connected to the data base!"
 
-@app.route('/addproduct/', methods=['POST'])
-def addproduct():
-    product = {
-        'name': request.json['name'],
-        'desc': request.json['desc']
-    }
-    db.products_collection.insert_one(product)
-    return redirect(url_for('getproducts'))
+# # the real deal
+# @app.route('/getproducts/', methods=['GET'])
+# def getproducts():
+#     products_json = []
+#     if db.products_collection.find({}):
+#         for product in db.products_collection.find({}).sort("name"):
+#             products_json.append({"id": str(product['_id']), "name": product['name']})
+#     return json.dumps(products_json)
+
+# @app.route('/addproduct/', methods=['POST'])
+# def addproduct():
+#     product = {
+#         'name': request.json['name'],
+#         'desc': request.json['desc']
+#     }
+#     db.products_collection.insert_one(product)
+#     return redirect(url_for('getproducts'))
 
 #-------------------------------------------
 
--- db.py
+++ db.py
@@ -1,9 +1,9 @@
-from flask import Flask
-from flask_pymongo import pymongo
+# from flask import Flask
+# from flask_pymongo import pymongo
 
-CONNECTION_STRING = "mongodb+srv://nikxtaco:qwerty25@cluster0.2jgti.mongodb.net/storage?retryWrites=true&w=majority"
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('storage')
-products_collection = pymongo.collection.Collection(db, 'products')
-users_collection = pymongo.collection.Collection(db, 'users')
-orders_collection = pymongo.collection.Collection(db, 'orders')
+# CONNECTION_STRING = "mongodb+srv://nikxtaco:qwerty25@cluster0.2jgti.mongodb.net/storage?retryWrites=true&w=majority"
+# client = pymongo.MongoClient(CONNECTION_STRING)
+# db = client.get_database('storage')
+# products_collection = pymongo.collection.Collection(db, 'products')
+# users_collection = pymongo.collection.Collection(db, 'users')
+# orders_collection = pymongo.collection.Collection(db, 'orders')
