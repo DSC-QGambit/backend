2312c1d16ff512b49b9b692d867822edad905031
nikxtaco <nikitamenon2510@gmail.com>
2023-11-23 10:25:25 +0530

    Minor edits

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_cors import CORS, cross_origin
 import json
 from views import index
 import sys
-import json
 from time import mktime
 import nltk
 from datetime import datetime
@@ -12,8 +11,10 @@ import newspaper
 from newspaper import Article
 
 from sentence_transformers import SentenceTransformer, util
-from transformers import AutoTokenizer, AutoModel
-import torch
+from transformers import pipeline
+import praw
+from praw.models import MoreComments
+from sklearn.cluster import KMeans
 
 nltk.download('punkt')
 
@@ -26,13 +27,13 @@ app.config['CORS_HEADERS']='Content-Type'
 @cross_origin()
 def pg():
     index()
-    return "k"
+    return "This is the backend for filter bubble."
 
 ### NEWS PAPER SCRAPING
 
 data = {}
 data["newspapers"] = {}
-max_articles_from_single_source_limit = 10
+max_articles_from_single_source_limit = 4
 
 def parse_config(fname):
     # Loads the JSON files with news sites
@@ -56,10 +57,11 @@ def _handle_rss(company, value, count, limit):
     # print(f"Downloading articles from {company}")
     news_paper = {"rss": value["rss"], "link": value["link"], "articles": []}
     for entry in fpd.entries:
+        # print(f'entry: {entry}')
         # Check if publish date is provided, if no the article is
         # skipped.  This is done to keep consistency in the data and to
         # keep the script from crashing.
-        if not hasattr(entry, "published"):
+        if (not hasattr(entry, "published")) or (entry["title"] == "") or (not hasattr(entry, "top_image")) or (entry["text"] == "") or (entry["keywords"] == []):
             continue
         if count > limit:
             break
@@ -82,7 +84,7 @@ def _handle_rss(company, value, count, limit):
         article["keywords"] = content.keywords
         article["top_image"] = content.top_image
         news_paper["articles"].append(article)
-        # print(f"{count} articles downloaded from {company}, url: {entry.link}")
+        print(f"{count} articles downloaded from {company}, url: {entry.link}")
         count = count + 1
     return count, news_paper
 
@@ -108,7 +110,7 @@ def _handle_fallback(company, value, count, limit):
             continue
         # If there is no found publish date the article will be skipped.
         # After 10 downloaded articles from the same newspaper without publish date, the company will be skipped.
-        if content.publish_date is None:
+        if content.publish_date is None or content.title == "" or content.top_image == "" or content.text == "" or content.keywords == []:
             # print(f"{count} Article has date of type None...")
             none_type_count = none_type_count + 1
             if none_type_count > 10:
@@ -134,7 +136,7 @@ def _handle_fallback(company, value, count, limit):
     return count, news_paper
 
 
-def run(config, limit=4):
+def run(config):
     """Take a config object of sites and urls, and an upper limit. Iterate through each news company.
     Write result to scraped_articles.json."""
     for company, value in config.items():
@@ -164,21 +166,21 @@ def news():
 
 ### Helper functions
 
-def summarise_article(text):
-    ## Using SUmy package
-    from sumy.parsers.plaintext import PlaintextParser
-    from sumy.nlp.tokenizers import Tokenizer
-    from sumy.summarizers.lex_rank import LexRankSummarizer
+# def summarise_article(text):
+#     ## Using SUmy package
+#     from sumy.parsers.plaintext import PlaintextParser
+#     from sumy.nlp.tokenizers import Tokenizer
+#     from sumy.summarizers.lex_rank import LexRankSummarizer
 
-    parser = PlaintextParser.from_string(text, Tokenizer("english"))
-    summarizer = LexRankSummarizer()
-    #Summarize the document with 4 sentences
-    summary = summarizer(parser.document, 4)
-    summary_concat = ""
-    for sentence in summary:
-        summary_concat = summary_concat + ' ' + str(sentence)
+#     parser = PlaintextParser.from_string(text, Tokenizer("english"))
+#     summarizer = LexRankSummarizer()
+#     #Summarize the document with 4 sentences
+#     summary = summarizer(parser.document, 4)
+#     summary_concat = ""
+#     for sentence in summary:
+#         summary_concat = summary_concat + ' ' + str(sentence)
 
-    return summary_concat
+#     return summary_concat
 
 ### APIS
 
@@ -187,7 +189,6 @@ def summarise_article(text):
 @app.route("/get-top-news-articles/", methods=['GET'])
 @cross_origin()
 def get_top_news_articles():
-
     news()
     articles_list = []
     with open("scraped_articles.json", "r") as data_file:
@@ -200,8 +201,6 @@ def get_top_news_articles():
             else:
                 for article in link_and_articles['articles']:
                     article['source'] = str(list(eachpaper.keys())[0])
-                    # article['text']=summarise_article(article['text'])
-
                     articles_list.append(article)
             
     return jsonify(articles_list)
@@ -210,11 +209,13 @@ def get_top_news_articles():
 @app.route("/get-article-summary/", methods=['GET', 'POST'])
 @cross_origin()
 def get_article_summary():
-    from transformers import pipeline
-
     summarizer = pipeline("summarization", model="mrm8488/t5-base-finetuned-summarize-news", tokenizer="mrm8488/t5-base-finetuned-summarize-news", framework="pt")
-    summary = summarizer(request.get_json(), min_length=5, max_length=500)
-    
+    if len(request.get_json()) >= 500:
+        max_len = 500
+    else:
+        max_len = len(request.get_json())
+    summary = summarizer(request.get_json(), min_length=5, max_length=max_len)
+
     summary = summary[0]['summary_text']
     last_period_index = summary.rfind('.')
     if last_period_index != -1:
@@ -227,30 +228,31 @@ def get_article_summary():
 
 @app.route("/get-related-articles/", methods=['GET','POST'])
 def get_related_articles():
-    import praw
     reddit_read_only = praw.Reddit(client_id="oVMUat1BYXBgQw-ksed5Hg",         # your client id
                                 client_secret="CKVIiUs4Ma07bP31gSZ3jt0hMqD0AQ",    # your client secret
                                 user_agent="News_Subreddit_Crawler")   # your user agent
     related_reddit_posts = []
+    limit = 10
+    count = 0
 
     if request.method == 'POST':
         for keyword in request.get_json()['keywords']:
-            print(keyword)
             listing = reddit_read_only.subreddit("news").search(keyword)
-            # subreddit = reddit_read_only.subreddit("all").search("OpenAI")
-            print('---')
 
             for id in listing:
                 post = reddit_read_only.submission(id=id)
                 related_reddit_posts.append([post.title, post.url])
-            break
+                count += 1
 
-        print(related_reddit_posts)
+            if(count == limit):
+                break
+
+    print(related_reddit_posts)
     return jsonify(related_reddit_posts)
+
 
 @app.route("/get-selected-news-keywords/", methods=['GET'])
 def get_selected_news_keywords():
-
     keywords_list = []
     with open("scraped_articles.json", "r") as data_file:
         scraped_file = json.load(data_file)
@@ -269,9 +271,6 @@ def get_selected_news_keywords():
 
 @app.route("/get-public-opinion-from-reddit/", methods=['GET','POST'])
 def get_public_opinion_from_reddit():
-    import praw
-    from praw.models import MoreComments
-
     titles = []
     ids = []
     urls = []
@@ -304,7 +303,6 @@ def get_public_opinion_from_reddit():
             if(comment.body != '[removed]'):
                 post_comments.append(comment.body)
 
-        from transformers import pipeline
         classifier = pipeline(model="distilbert-base-uncased-finetuned-sst-2-english")
 
         positive_sum, negative_sum, neutral_sum = 0, 0, 0
@@ -327,11 +325,14 @@ def get_public_opinion_from_reddit():
         public_sentiments['neg'] = negative_sum / len(post_comments)
         public_sentiments['neu'] = neutral_sum / len(post_comments)
 
-        ## K-Means
-        from sklearn.cluster import KMeans
+        ### K-Means
 
         post_comments_embeddings = model.encode(post_comments)
-        num_clusters = 2
+        if len(post_comments)>2:
+            num_clusters = 2
+        else:
+            num_clusters = len(post_comments)
+
         clustering_model = KMeans(n_clusters=num_clusters)
         clustering_model.fit(post_comments_embeddings)
         cluster_assignment = clustering_model.labels_
@@ -343,16 +344,18 @@ def get_public_opinion_from_reddit():
         summarizer = pipeline("summarization", model="mrm8488/t5-base-finetuned-summarize-news", tokenizer="mrm8488/t5-base-finetuned-summarize-news", framework="pt")
 
         for i, cluster in enumerate(clustered_sentences):
-            # print("Cluster ", i+1)
             cluster_string = ' '.join(cluster)
-            summary = summarizer(cluster_string, min_length=5, max_length=300)
+            if len(cluster_string) >= 300:
+                max_len = 300
+            else:
+                max_len = len(cluster_string)
+            summary = summarizer(cluster_string, min_length=5, max_length=max_len)
             summary = summary[0]['summary_text']
-            print(summary)
+
             last_period_index = summary.rfind('.')
             if last_period_index != -1:
                 summary = summary[:last_period_index + 1]
             public_sentiments['summary'].append(summary)
-
 
         all_public_sentiments.append(public_sentiments)
 
